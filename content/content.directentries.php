7caab064c02c82744f5802a56bf0167065aa2ab1
vlad <vlad_micutul@yahoo.com>
2013-12-10 06:45:43 +0200

    Keyword filtering works

--- content/content.directentries.php
+++ content/content.directentries.php
@@ -3,7 +3,7 @@
 
 	require_once(TOOLKIT . '/class.administrationpage.php');
 
-	class contentExtensionRelation_fieldDirectEntries extends AdministrationPage {
+	class contentExtensionRelationsDirectEntries extends AdministrationPage {
 
 		public function view() {
 			$keywords = Mysql::cleanValue($_REQUEST['keywords']);
@@ -11,6 +11,7 @@
 			$per_page = Mysql::cleanValue($_REQUEST['per-page']);
 			$field_id = Mysql::cleanValue($_REQUEST['field-id']);
 
+			// @todo set correct field
 			/** @var FieldRelation $field */
 //			$field = FieldManager::fetch($field_id);
 
@@ -19,36 +20,83 @@
 //				$this->returnResult(array(0));
 //			}
 
-			list($where, $join) = $this->applyKeywordFilter($keywords);
-
-			$section_id = SectionManager::fetchIDFromHandle('images');
-//			$section_id = $field->get('parent_section');
-
-			$entries = EntryManager::fetchByPage(
-			                       $page,
-			                       $section_id,
-			                       $per_page === 'all' ? null : $per_page,
-			                       $where,
-			                       $join,
-			                       false,
-			                       false,
-			                       true,
-			                       array('title', 'image', 'caption')
+			// @todo set correct section IDs from Field
+			$section_ids = array(
+				SectionManager::fetchIDFromHandle('images'),
+				SectionManager::fetchIDFromHandle('images')
 			);
+
+			$entries = array();
+
+			foreach ($section_ids as $section_id) {
+
+				// @todo set correct related fields
+				list($where, $joins) = $this->applyKeywordFilter(
+				                            $field_ids = array(
+					                            1, // title
+					                            2, // image
+					                            4, // caption
+				                            ),
+				                            $keywords
+				);
+
+				$entries[$section_id] = EntryManager::fetchByPage(
+				                                    $page,
+				                                    $section_id,
+				                                    $per_page === 'all' ? null : $per_page,
+				                                    $where,
+				                                    $joins,
+				                                    $group = false,
+				                                    $record_only = false,
+				                                    $build_entries = true,
+//								                    @todo set correct schema
+				                                    $schema_elements = array('title', 'image', 'caption')
+				);
+			}
 
 			print_r($entries);
 
-			$this->returnResult($entries);
+//			$this->returnResult($entries);
 
-			echo 1;
 			die;
 		}
 
-		private function applyKeywordFilter($keywords) {
+		/**
+		 * Applies keywords filters and returns $where and $joins
+		 *
+		 * @param $field_ids - IDs of targeted fields
+		 * @param $keywords  - keywords to search for
+		 *
+		 * @return array
+		 */
+		private function applyKeywordFilter($field_ids, $keywords) {
 			$where = '';
-			$join  = '';
+			$joins = '';
 
-			return array($where, $join);
+			$filter = array("regexp: $keywords");
+
+			foreach ($field_ids as $field_id) {
+				/** @var Field $field */
+				$field = FieldManager::fetch($field_id);
+
+				if (method_exists($field, 'buildDSRetrievalSQL')) {
+					$field->buildDSRetrievalSQL($filter, $joins, $where_field, false);
+				}
+				else {
+					$field->buildDSRetrivalSQL($filter, $joins, $where_field, false);
+				}
+
+				// replace all ANDs with ORs
+				$where_field = preg_replace('/^AND/', 'OR', trim($where_field));
+				$where_field = preg_replace('/\) *AND *\(/', ') OR (', trim($where_field));
+
+				$where .= ' ' . $where_field;
+			}
+
+			// make this whole thing required
+			$where = preg_replace('/^OR/', 'AND (', trim($where)) . ')';
+
+			return array($where, $joins);
 		}
 
 		private function returnResult($result) {
--- extension.driver.php
+++ extension.driver.php
@@ -10,9 +10,10 @@
 			return Symphony::Database()->query("CREATE TABLE IF NOT EXISTS `tbl_fields_relation` (
 				`id` int(11) unsigned NOT NULL auto_increment,
 				`field_id` int(11) unsigned NOT NULL,
-				`related_field_id` VARCHAR(255) NOT NULL,
 				`limit` int(6) unsigned NOT NULL default '0',
-				`views` TEXT NOT NULL default '',
+				`sections` VARCHAR(255) NOT NULL default '',
+				`view` VARCHAR(255) NOT NULL default '',
+				`view_info` TEXT NOT NULL default '',
 				PRIMARY KEY  (`id`),
 				KEY `field_id` (`field_id`)
             ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;");
